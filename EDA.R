6060d5c91d3103f9f75385ef3721e6c6074e61cd
Kalicz Péter <kaliczp@gmail.com>
2024-10-17 12:11:45 +0200

    Work with indexing

--- EDA.R
+++ EDA.R
@@ -1,3 +1,22 @@
 ## Vektor data.frame-bő.
-lab1$Ca..mg.g.
-lab1$Ca...mg.g...sz.a..
+summary(lab1$Ca..mg.g.)
+summary(lab1$Ca...mg.g...sz.a..)
+
+## Vektor első eleme
+lab1$Ca...mg.g...sz.a..[1]
+## Az első 10 elem kettőspont operátorral
+lab1$Ca...mg.g...sz.a..[1:10]
+## Ugyan ez vektorral
+lab1$Ca...mg.g...sz.a..[c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+## Vétletlen szerű elemek
+lab1$Ca...mg.g...sz.a..[c(1, 2, 3, 4, 5, 6, 7, 80, 90, 100)]
+## Negatív index
+lab1$Ca...mg.g...sz.a..[-854]
+lab1$Ca...mg.g...sz.a..[-c(852:854)]
+lab1$Ca...mg.g...sz.a..[-c(406:854)]
+## Logikai kifejezssel
+lab1$Ca...mg.g...sz.a.. > 0
+lab1$Ca...mg.g...sz.a..[lab1$Ca...mg.g...sz.a.. > 0]
+is.na(lab1$Ca...mg.g...sz.a..)
+!is.na(lab1$Ca...mg.g...sz.a..)
+lab1$Ca...mg.g...sz.a..[lab1$Ca...mg.g...sz.a.. > 0 & !is.na(lab1$Ca...mg.g...sz.a..)]
