401384e289923cb90ae06170ac5c1e120ce9bd11
Kalicz Péter <kaliczp@gmail.com>
2024-12-10 14:00:34 +0100

    describe

--- Question.R
+++ Question.R
@@ -1,6 +1,16 @@
-## 
+## Csomag telepítése
 install.packages("questionr")
+## Csomag betöltése
 
+library(questionr)
+help.start()
+## fertility kézikönyve adat nézegetés
+data(fertility)
+households
+as.data.frame(households)
+describe(households)
+describe(women)
+describe(women$residency)
 
 ## Feldolgozáshoz
 install.packages("Dire")
